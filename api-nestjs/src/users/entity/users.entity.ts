f1a24fa2fa354f06afefcabba2c7878c14d6fe9a
ricardogitalc <alcantara.ricardo00@gmail.com>
2024-11-19 19:45:07 -0300

    imageUrl field

--- api-nestjs/src/users/entity/users.entity.ts
+++ api-nestjs/src/users/entity/users.entity.ts
@@ -1,11 +1,12 @@
 export class User {
+  role: string;
   id: number;
   email: string;
   firstName: string;
   lastName: string;
   whatsappNumber?: string;
+  imageUrl?: string;
   verified: boolean;
   createdAt?: Date;
   updatedAt?: Date;
-  role: string;
 }
